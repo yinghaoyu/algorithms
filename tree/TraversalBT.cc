7efcd41a370ee963dd59b54c0ca26d457547843d
rain <20520@126.com>
2022-05-05 14:03:49 +0800

    add summarize in TraversalBT

--- tree/TraversalBT.cc
+++ tree/TraversalBT.cc
@@ -11,6 +11,15 @@ typedef struct Node
   Node* right;
 }Node;
 
+// 在递归序的基础上，只打印第一次经过自己时的值，即是先序；
+// 只打印第二次经过自己的值，即是中序；
+// 只打印第三次经过自己的值，即是后序。
+
+// 一个结论：已知一个二叉树的先序遍历和后序遍历，
+// 某个节点X，X先序遍历之前的节点集合为A，
+// X后序遍历之后的节点集合为B，
+// 那么 A 和 B 的交集一定是X节点的所有祖先节点。
+
 // Recursive先序
 void pre(Node* head)
 {
@@ -48,6 +57,10 @@ void pos(Node* head)
 }
 
 // UnRecursive先序
+//（1）准备一个栈，压入当前节点（即头节点）
+//（2）弹出栈顶元素，打印对应的值
+//（3）此元素有右孩子往栈压入右孩子，有左孩子往栈压入左孩子（先右再左）
+//（4）一直执行2、3步，直到栈为空。
 void preUnRe(Node* head)
 {
   if (head != NULL)
@@ -72,6 +85,10 @@ void preUnRe(Node* head)
 }
 
 // UnRecursive中序
+//（1）准备一个栈
+//（2）压入以当前节点current为头节点的整个左子树（入栈一个，current就移动到左孩子），直到为空
+//（3）弹出栈顶元素，打印其值，以当前弹出元素的右孩子为current节点，重复第2步
+//（4）当栈为空时结束
 void inUnRe(Node* cur)
 {
   if (cur != NULL) {
@@ -95,6 +112,12 @@ void inUnRe(Node* cur)
 }
 
 // UnRecursive后序
+//（1）准备两个栈stackA，stackB；stackA压入当前节点（即头节点）
+//（2）弹出栈顶元素，压入stackB
+//（3）此元素有左孩子往stackA栈压入左孩子，有右孩子往stackA栈压入右孩子（先左再右）
+//（4）一直执行2、3步，直到stackA栈为空。
+//（5）打印所有stackB栈的元素
+// 相当于stackA出栈顺序是 根右左，最后stackB出栈顺序是 左右根。
 void pos1(Node* head)
 {
   if (head != NULL)
@@ -105,7 +128,7 @@ void pos1(Node* head)
     while (!s1.empty())
     {
       head = s1.back();
-      s1.pop_back(); // 头 右 左
+      s1.pop_back(); // 跟 右 左
       s2.push_back(head);
       if (head->left != NULL)
       {
@@ -116,7 +139,7 @@ void pos1(Node* head)
         s1.push_back(head->right);
       }
     }
-    // 左 右 头
+    // 左 右 跟
     while (!s2.empty()) {
       Node* cur = s2.back();
       s2.pop_back();
@@ -153,7 +176,7 @@ void pos2(Node* head)
   }
 }
 
-// bfs
+// bfs层序遍历
 void level(Node* head)
 {
   if (head == NULL)
