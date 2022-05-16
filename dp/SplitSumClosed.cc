389558496ad0e51c8145d4fe925772164d483afc
rain <20520@126.com>
2022-05-16 15:48:18 +0800

    add SplitSumClosed

---
+++ dp/SplitSumClosed.cc
@@ -0,0 +1,172 @@
+#include <random>
+#include <memory.h>
+#include <iostream>
+#include <stdbool.h>
+
+using namespace std;
+
+class SplitSumClosed
+{
+  public:
+
+    static int right(int* arr, int len)
+    {
+      if (arr == nullptr || len < 2)
+      {
+        return 0;
+      }
+      int sum = 0;
+      for (int i  = 0; i < len ;i++)
+      {
+        sum += arr[i];
+      }
+      return process(arr, len,  0, sum / 2);
+    }
+
+    // arr[i...]可以自由选择，请返回累加和尽量接近rest，但不能超过rest的情况下，最接近的累加和是多少？
+    static int process(int* arr, int len, int i, int rest)
+    {
+      if (i == len)
+      {
+        return 0;
+      }
+      else
+      {
+        // 还有数，arr[i]这个数
+        // 可能性1，不使用arr[i]
+        int p1 = process(arr, len,  i + 1, rest);
+        // 可能性2，要使用arr[i]
+        int p2 = 0;
+        if (arr[i] <= rest)
+        {
+          p2 = arr[i] + process(arr, len, i + 1, rest - arr[i]);
+        }
+        return std::max(p1, p2);
+      }
+    }
+
+    static int dp(int* arr, int len)
+    {
+      if (arr == nullptr || len < 2)
+      {
+        return 0;
+      }
+      int sum = 0;
+      for (int i = 0; i < len; i++)
+      {
+        sum += arr[i];
+      }
+      sum /= 2;
+      int N = len;
+      int** dp = mallocArray(N + 1, sum + 1);
+      for (int i = N - 1; i >= 0; i--)
+      {
+        for (int rest = 0; rest <= sum; rest++)
+        {
+          // 可能性1，不使用arr[i]
+          int p1 = dp[i + 1][rest];
+          // 可能性2，要使用arr[i]
+          int p2 = 0;
+          if (arr[i] <= rest)
+          {
+            p2 = arr[i] + dp[i + 1][rest - arr[i]];
+          }
+          dp[i][rest] = std::max(p1, p2);
+        }
+      }
+      int ans = dp[0][sum];
+      freeArray(dp, N + 1);
+      return ans;
+    }
+
+    static int* randomArray(int len, int value)
+    {
+      int* arr = (int*)malloc(sizeof(int) * len);
+      for (int i = 0; i < len; i++)
+      {
+        arr[i] = getRandom(0, value);
+      }
+      return arr;
+    }
+
+    static void printArray(int* arr, int len)
+    {
+      for (int i = 0; i < len; i++)
+      {
+        cout << arr[i] << " ";
+      }
+      cout << endl;
+    }
+
+    // for test
+    static int** mallocArray(int row, int column)
+    {
+      int** arr = (int**)malloc(sizeof(int*) * row);
+      for(int i = 0; i < row; i++)
+      {
+        arr[i] = (int*)malloc(sizeof(int) * column);
+        memset(arr[i], 0, sizeof(int) * column);
+      }
+      return arr;
+    }
+
+    static void freeArray(int** arr, int row)
+    {
+      for(int i = 0; i < row; i++)
+      {
+        free(arr[i]);
+      }
+      free(arr);
+    }
+
+    // 为了测试
+    static int* randomArray(int maxLen, int maxValue, int* len)
+    {
+      *len = getRandom(0, maxLen);
+      int* arr = (int*)malloc(sizeof(int) * (*len));
+      for (int i = 0; i < *len; i++)
+      {
+        arr[i] = getRandom(1, maxValue);
+      }
+      return arr;
+    }
+
+    static int getRandom(int min, int max)
+    {
+      random_device seed;  // 硬件生成随机数种子
+      ranlux48 engine(seed());  // 利用种子生成随机数引
+      uniform_int_distribution<> distrib(min, max);  // 设置随机数范围，并为均匀分布
+      int res = distrib(engine);  // 随机数
+      return res;
+    }
+
+    static void test()
+    {
+      int maxLen = 20;
+      int maxValue = 50;
+      int testTime = 10000;
+      cout << "测试开始" << endl;
+      for (int i = 0; i < testTime; i++)
+      {
+        int len = getRandom(0, maxLen);
+        int* arr = randomArray(len, maxValue);
+        int ans1 = right(arr, len);
+        int ans2 = dp(arr, len);
+        if (ans1 != ans2)
+        {
+          printArray(arr, len);
+          cout << ans1 << endl;
+          cout << ans2 << endl;
+          cout << "Oops!" << endl;
+          break;
+        }
+      }
+      cout << "测试结束" << endl;
+    }
+};
+
+int main()
+{
+  SplitSumClosed::test();
+  return 0;
+}
