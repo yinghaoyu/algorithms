75be59be4d6bf56da9dde60b2c6c429d1581f773
rain <20520@126.com>
2022-06-20 10:23:02 +0800

    turn dynamic array to vector

--- sort/BiggerThanRightTwice.cc
+++ sort/BiggerThanRightTwice.cc
@@ -1,6 +1,7 @@
-#include <iostream>
 #include <stdbool.h>
+#include <iostream>
 #include <random>
+#include <vector>
 
 using namespace std;
 
@@ -12,159 +13,158 @@ using namespace std;
 
 class BiggerThanRightTwice
 {
-  public:
-    static int getRandom(int min, int max)
-    {
-      random_device seed;  // 硬件生成随机数种子
-      ranlux48 engine(seed());  // 利用种子生成随机数引
-      uniform_int_distribution<> distrib(min, max);  // 设置随机数范围，并为均匀分布
-      int res = distrib(engine);  // 随机数
-      return res;
-    }
+ public:
+  static int getRandom(int min, int max)
+  {
+    random_device seed;                            // 硬件生成随机数种子
+    ranlux48 engine(seed());                       // 利用种子生成随机数引
+    uniform_int_distribution<> distrib(min, max);  // 设置随机数范围，并为均匀分布
+    int res = distrib(engine);                     // 随机数
+    return res;
+  }
 
-    static int biggerTwice(int arr[], int len)
+  static int biggerTwice(vector<int> &arr)
+  {
+    if (arr.size() < 2)
     {
-      if (arr == nullptr || len < 2)
+      return 0;
+    }
+    return process(arr, 0, arr.size() - 1);
+  }
+
+  static int process(vector<int> &arr, int l, int r)
+  {
+    if (l == r)
+    {
+      return 0;
+    }
+    // l < r
+    int mid = l + ((r - l) >> 1);
+    return process(arr, l, mid) + process(arr, mid + 1, r) + merge(arr, l, mid, r);
+  }
+
+  static int merge(vector<int> &arr, int L, int m, int r)
+  {
+    // [L....M]   [M+1....R]
+    int ans = 0;
+    // 目前囊括进来的数，是从[M+1, windowR)
+    int windowR = m + 1;
+    for (int i = L; i <= m; i++)
+    {
+      while (windowR <= r && arr[i] > (arr[windowR] << 1))
       {
-        return 0;
+        windowR++;
       }
-      return process(arr, 0, len - 1);
+      ans += windowR - m - 1;  // arr[windowR]是第一个不满足条件的
     }
 
-    static int process(int arr[], int l, int r)
+    int len = r - L + 1;
+    vector<int> help(len);
+    int i = 0;
+    int p1 = L;
+    int p2 = m + 1;
+    while (p1 <= m && p2 <= r)
     {
-      if (l == r)
-      {
-        return 0;
-      }
-      // l < r
-      int mid = l + ((r - l) >> 1);
-      return process(arr, l, mid)
-        + process(arr, mid + 1, r)
-        + merge(arr, l, mid, r);
+      help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];  // 这里<=不影响结果，为了保证稳定，先拷贝左数组
     }
-
-    static int merge(int arr[], int L, int m, int r)
+    while (p1 <= m)
     {
-      // [L....M]   [M+1....R]
-      int ans = 0;
-      // 目前囊括进来的数，是从[M+1, windowR)
-      int windowR = m + 1;
-      for (int i = L; i <= m; i++)
+      help[i++] = arr[p1++];
+    }
+    while (p2 <= r)
+    {
+      help[i++] = arr[p2++];
+    }
+    for (i = 0; i < len; i++)
+    {
+      arr[L + i] = help[i];
+    }
+    return ans;
+  }
+
+  // for test
+  // 暴力解时间复杂度O(N^2)
+  static int comparator(vector<int> &arr)
+  {
+    int ans = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+      for (int j = i + 1; j < arr.size(); j++)
       {
-        while (windowR <= r && arr[i] > (arr[windowR] << 1))
+        if (arr[i] > (arr[j] << 1))
         {
-          windowR++;
-        }
-        ans += windowR - m - 1;
-      }
-
-      int len = r - L + 1;
-      int* help = new int[len];
-      int i = 0;
-      int p1 = L;
-      int p2 = m + 1;
-      while (p1 <= m && p2 <= r)
-      {
-        help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++]; // 这里<=不影响结果，为了保证稳定，先拷贝左数组
-      }
-      while (p1 <= m)
-      {
-        help[i++] = arr[p1++];
-      }
-      while (p2 <= r)
-      {
-        help[i++] = arr[p2++];
-      }
-      for (i = 0; i < len; i++)
-      {
-        arr[L + i] = help[i];
-      }
-      return ans;
-    }
-
-    // for test
-    static int comparator(int arr[], int len)
-    {
-      int ans = 0;
-      for (int i = 0; i < len; i++)
-      {
-        for (int j = i + 1; j < len; j++)
-        {
-          if (arr[i] > (arr[j] << 1))
-          {
-            ans++;
-          }
+          ans++;
         }
       }
-      return ans;
     }
+    return ans;
+  }
 
-    // for test
-    static int* generateRandomArray(int maxSize, int maxValue, int* len)
+  // for test
+  static vector<int> generateRandomArray(int maxSize, int maxValue, int *len)
+  {
+    *len = getRandom(0, maxSize);
+    vector<int> arr(*len);
+    for (int i = 0; i < *len; i++)
     {
-      *len = getRandom(0, maxSize);
-      int* arr = new int[*len];
-      for (int i = 0; i < *len; i++)
-      {
-        arr[i] = getRandom(0, maxValue) - getRandom(0, maxValue);
-      }
-      return arr;
+      arr[i] = getRandom(0, maxValue) - getRandom(0, maxValue);
     }
+    return arr;
+  }
 
-    // for test
-    static int* copyArray(int* arr, int len)
+  // for test
+  static int *copyArray(int *arr, int len)
+  {
+    if (arr == nullptr)
     {
-      if (arr == nullptr)
-      {
-        return nullptr;
-      }
-      int* res = new int[len];
-      for (int i = 0; i < len; i++)
-      {
-        res[i] = arr[i];
-      }
-      return res;
+      return nullptr;
     }
-
-    // for test
-    static bool isEqual(int arr1[], int arr2[], int len1, int len2)
+    int *res = new int[len];
+    for (int i = 0; i < len; i++)
     {
-      if ((arr1 == nullptr && arr2 != nullptr) || (arr1 != nullptr && arr2 == nullptr))
-      {
-        return false;
-      }
-      if (arr1 == nullptr && arr2 == nullptr)
-      {
-        return true;
-      }
-      if (len1 != len2)
-      {
-        return false;
-      }
-      for (int i = 0; i < len1; i++)
-      {
-        if (arr1[i] != arr2[i])
-        {
-          return false;
-        }
-      }
+      res[i] = arr[i];
+    }
+    return res;
+  }
+
+  // for test
+  static bool isEqual(int arr1[], int arr2[], int len1, int len2)
+  {
+    if ((arr1 == nullptr && arr2 != nullptr) || (arr1 != nullptr && arr2 == nullptr))
+    {
+      return false;
+    }
+    if (arr1 == nullptr && arr2 == nullptr)
+    {
       return true;
     }
-
-    // for test
-    static void printArray(int arr[], int len)
+    if (len1 != len2)
     {
-      if (arr == nullptr)
-      {
-        return;
-      }
-      for (int i = 0; i < len; i++)
-      {
-        cout<< arr[i] << " ";
-      }
-      cout << endl;
+      return false;
     }
+    for (int i = 0; i < len1; i++)
+    {
+      if (arr1[i] != arr2[i])
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // for test
+  static void printArray(vector<int> &arr)
+  {
+    if (arr.size() == 0)
+    {
+      return;
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+      cout << arr[i] << " ";
+    }
+    cout << endl;
+  }
 };
 
 int main()
@@ -176,13 +176,13 @@ int main()
   int len = 0;
   for (int i = 0; i < testTime; i++)
   {
-    int* arr1 = BiggerThanRightTwice::generateRandomArray(maxSize, maxValue, &len);
-    int* arr2 = BiggerThanRightTwice::copyArray(arr1, len);
-    if (BiggerThanRightTwice::biggerTwice(arr1, len) != BiggerThanRightTwice::comparator(arr2, len))
+    vector<int> arr1 = BiggerThanRightTwice::generateRandomArray(maxSize, maxValue, &len);
+    vector<int> arr2 = arr1;
+    if (BiggerThanRightTwice::biggerTwice(arr1) != BiggerThanRightTwice::comparator(arr2))
     {
       succeed = false;
-      BiggerThanRightTwice::printArray(arr1, len);
-      BiggerThanRightTwice::printArray(arr2, len);
+      BiggerThanRightTwice::printArray(arr1);
+      BiggerThanRightTwice::printArray(arr2);
       break;
     }
   }
