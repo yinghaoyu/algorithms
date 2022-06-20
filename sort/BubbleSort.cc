47c69c1a02b0175dec3db01c798f4bc2ab014923
rain <20520@126.com>
2022-06-20 14:39:52 +0800

    turn dynamic array to vector

--- sort/BubbleSort.cc
+++ sort/BubbleSort.cc
@@ -1,16 +1,15 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 // 冒泡排序
 // 时间复杂度O(n^2)
 // 空间复杂度O(1)
 // 稳定性：可以达到稳定
 
-#define N 1000
-
-int arr[N];
-
 // 交换arr的i和j位置上的值
-void swap(int arr[], int i, int j)
+void swap(vector<int> &arr, int i, int j)
 {
   if (i == j)
   {
@@ -21,16 +20,18 @@ void swap(int arr[], int i, int j)
   arr[i] = arr[i] ^ arr[j];
 }
 
-void bubbleSort(int arr[]) {
-  if (arr == nullptr || N < 2)
+void bubbleSort(vector<int> &arr)
+{
+  if (arr.size() < 2)
   {
     return;
   }
   // 0 ~ N-1
   // 0 ~ N-2
   // 0 ~ N-3
-  for (int e = N - 1; e > 0; e--)
-  { // 0 ~ e
+  for (int e = arr.size() - 1; e > 0; e--)
+  {
+    // 0 ~ e
     for (int i = 0; i < e; i++)
     {
       if (arr[i] > arr[i + 1])
