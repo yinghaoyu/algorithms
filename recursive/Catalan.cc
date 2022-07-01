93203996869a41e7f699ab7c73ccdaffef82df07
rain <20520@126.com>
2022-07-01 16:06:02 +0800

    add description

--- recursive/Catalan.cc
+++ recursive/Catalan.cc
@@ -3,22 +3,26 @@
 
 using namespace std;
 
+// 问题描述：n个元素进栈，共有多少种出栈顺序？
+
 // 卡特兰数组
+// https://zhuanlan.zhihu.com/p/97619085
 
 class Catalan
 {
  public:
+  // 根据性质计算
   static long ways1(int N)
   {
-    int zero = N;
+    int zero = N;  // 合法的序列1和0应该数量一样
     int one = N;
-    list<int> path;
-    list<list<int>> ans;
+    list<int> path;       // 尝试扩1或者扩0的路径
+    list<list<int>> ans;  // 所有路径集合
     process(zero, one, path, ans);
     long count = 0;
     for (list<int> cur : ans)
     {
-      int status = 0;
+      int status = 0;  // 用来统计前缀和1和0的数量差
       for (int num : cur)
       {
         if (num == 0)
@@ -29,12 +33,12 @@ class Catalan
         {
           status--;
         }
-        if (status < 0)
+        if (status < 0)  // 前缀和只要有一次 < 0，这个路径就是非法路径
         {
           break;
         }
       }
-      if (status == 0)
+      if (status == 0)  // 1和0数量一样，说明是合法路径
       {
         count++;
       }
@@ -46,22 +50,17 @@ class Catalan
   {
     if (zero == 0 && one == 0)
     {
-      list<int> cur;
-      for (int num : path)
-      {
-        cur.push_back(num);
-      }
-      ans.push_back(cur);
+      ans.push_back(path);
     }
     else
     {
-      if (zero == 0)
+      if (zero == 0)  // 只有1了
       {
         path.push_back(1);
         process(zero, one - 1, path, ans);
         path.pop_back();
       }
-      else if (one == 0)
+      else if (one == 0)  // 只有0了
       {
         path.push_back(0);
         process(zero - 1, one, path, ans);
@@ -69,16 +68,17 @@ class Catalan
       }
       else
       {
-        path.push_back(1);
+        path.push_back(1);  // 扩1
         process(zero, one - 1, path, ans);
-        path.pop_back();
-        path.push_back(0);
+        path.pop_back();    // 扩1和扩0是互斥的，因此要恢复
+        path.push_back(0);  // 扩0
         process(zero - 1, one, path, ans);
-        path.pop_back();
+        path.pop_back();  // 恢复
       }
     }
   }
 
+  // 直接利用公式
   static long ways2(int N)
   {
     if (N < 0)
